8a55e4bbfd20ab6bd377b70a0c2c3449b6b44ac8
Susie <45341415+Haldevel@users.noreply.github.com>
2019-05-24 18:18:06 -0700

    ready to deploy

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const db = require("./models");
 const errorHandler = require("./controllers/error");
 const itemRoutes = require("./routes/items");
